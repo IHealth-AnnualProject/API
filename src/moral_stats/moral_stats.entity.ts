871bb2982857951472854f7ff57e6a55794472ca
Kalfaa <theo92320@hotmail.fr>
2020-04-15 19:35:21 +0200

    create user decorator and moral_stats

---
+++ src/moral_stats/moral_stats.entity.ts
@@ -0,0 +1,19 @@
+import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {UserProfileEntity} from "../userProfile/userProfile.entity";
+
+
+@Entity('moral_stats')
+export class MoralStatsEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @Column('int')
+    value: number;
+
+    @ManyToOne(type => UserProfileEntity, patient => patient.id)
+    patient: UserProfileEntity;
+
+}
