9a109231cb55003a19381dbe65520d740d8c1826
Kalfaa <theo92320@hotmail.fr>
2020-05-04 11:59:20 +0200

    fix une erreur de merde

--- src/friendRequest/friendRequest.dto.ts
+++ src/friendRequest/friendRequest.dto.ts
@@ -1,5 +1,5 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {FriendRequestState} from "./FriendRequest.entity";
+import {FriendRequestState} from "./friendRequest.entity";
 
 
 export class FriendRequestDTO {
